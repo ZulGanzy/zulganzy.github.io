4c7f8927b261acb0aea12592c1c2570d5b376003
ZulGanzy <zultest85@gmail.com>
2024-09-02 18:54:44 +0700

    -

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed("#auto-typed", {
+  strings: ["<strong>World</strong>", "<strong>I'm Joule</strong>"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+  loopCount: Infinity,
+});
